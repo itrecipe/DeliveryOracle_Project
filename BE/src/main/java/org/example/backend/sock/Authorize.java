8a2a29e8a2718864ec52f22ea6c75b04cddd76ab
IT Recipe <itrecipe95@gmail.com>
2024-10-29 18:55:19 +0900

    t
    t

--- BE/src/main/java/org/example/backend/sock/Authorize.java
+++
@@ -1,6 +0,0 @@
-package org.example.backend.sock;
-
-import org.springframework.web.servlet.HandlerInterceptor;
-
-public class Authorize implements HandlerInterceptor {
-}
--- BE/src/main/java/org/example/backend/store/except/StoreNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.example.backend.store.except;
-
-// StoreNotFoundException 예외 클래스
-public class StoreNotFoundException extends RuntimeException {
-    public StoreNotFoundException(String message) {
-        super(message);
-    }
-}
-
